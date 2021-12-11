c74e5c03d6ef77581e875aec3a578d79c9f329da
Breno Vasconcellos <dv.brenovasconcellos@gmail.com>
2021-12-11 10:42:35 -0300

    first commit

---
+++ BrenoDevCard/BrenoDevCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrenoDevCardApp.swift
+//  BrenoDevCard
+//
+//  Created by Breno Vasconcellos on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BrenoDevCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
